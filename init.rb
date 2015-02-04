3c4a2ceb80046858a8a8b5a9982cd6134d018f1e
Tobias Fischer <tobias.carl.fischer@gmail.com>
2015-02-05 00:40:41 +0100

    Markdown support

--- init.rb
+++ init.rb
@@ -5,18 +5,20 @@ Redmine::Plugin.register :redmine_emojibutton do
   name 'Redmine Emoji Button'
   author 'Akinori Tomita & Tobias Fischer'
   description "Enable github style emoji's in tickets and comments and choose emojis from a new editor button"
-  version '0.2.0'
+  version '0.3.0'
   url 'https://github.com/paginagmbh/redmine_emojibutton'
 end
 
 Rails.configuration.to_prepare do
   Rails.configuration.assets.paths << Emoji.images_path
 
-  unless Redmine::WikiFormatting::Textile::Formatter.included_modules.include? EmojiButtonPlugin::Formatter::Patch
-    Redmine::WikiFormatting::Textile::Formatter.send(:include, EmojiButtonPlugin::Formatter::Patch)
-  end
-  
-  unless Redmine::WikiFormatting::Textile::Helper.included_modules.include? EmojiButtonPlugin::Helper::Patch
-    Redmine::WikiFormatting::Textile::Helper.send(:include, EmojiButtonPlugin::Helper::Patch)
+  # send Emoji Patches to all wiki formatters available to be able to switch formatter without app restart
+  Redmine::WikiFormatting::format_names.each do |format|
+    unless Redmine::WikiFormatting::formatter_for(format).included_modules.include? EmojiButtonPlugin::Formatter::Patch
+      Redmine::WikiFormatting::formatter_for(format).send(:include, EmojiButtonPlugin::Formatter::Patch)
+    end
+    unless Redmine::WikiFormatting::helper_for(format).included_modules.include? EmojiButtonPlugin::Helper::Patch
+     Redmine::WikiFormatting::helper_for(format).send(:include, EmojiButtonPlugin::Helper::Patch)
+    end
   end
 end
--- lib/emojibutton_formatter_patch.rb
+++ lib/emojibutton_formatter_patch.rb
@@ -5,14 +5,28 @@ module EmojiButtonPlugin
         base.send(:include, EmojiButtonPlugin::Formatter)
         base.class_eval do
           unloadable # Send unloadable so it will not be unloaded in development
+          
+          # generic formatter patch: override :to_html method and call :inline_emoji afterwards
+          alias_method_chain :to_html, :redmine_emojibutton
         end
       end
     end
 
-    Redmine::WikiFormatting::Textile::Formatter::RULES << :inline_emoji
+    # Textile patch: add :inline_emoji to list of textile functions
+    # this is the correct way to do for Textile, but it doesn't work both together
+    # with the generic formatter patch > override :to_html
+    # 
+    #Redmine::WikiFormatting::Textile::Formatter::RULES << :inline_emoji
 
     private
+    
+    # generic formatter patch: call overridden :to_html function first, then :inline_emoji
+    def to_html_with_redmine_emojibutton
+      html = to_html_without_redmine_emojibutton
+      inline_emoji(html)
+    end
 
+    # emoji text substitution
     def inline_emoji(text)
       text.gsub!(/:([a-z0-9\+\-_]+):/) do |match|
         if Emoji.names.include?($1)
